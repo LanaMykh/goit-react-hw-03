774e7a4d65687a5a0a8554a3f7d9f1fc0655045f
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-11-05 08:14:14 +0200

    step 5

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { useId } from 'react';
+import * as Yup from 'yup';
 import styles from './ContactForm.module.css';
 
 const initial_Values = {
@@ -11,13 +12,28 @@ const ContactForm = ({ onAddContact }) => {
   const frmNameId = useId();
   const frmNumberId = useId();
 
+  const ContactFormkSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    number: Yup.string()
+      .min(3, 'Too short')
+      .max(50, 'Too long')
+      .required('Required'),
+  });
+
   const handleSubmit = (values, actions) => {
     onAddContact(values);
     actions.resetForm();
   };
 
   return (
-    <Formik initialValues={initial_Values} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initial_Values}
+      onSubmit={handleSubmit}
+      validationSchema={ContactFormkSchema}
+    >
       <Form className={styles.form}>
         <label htmlFor={frmNameId} className={styles.label}>
           Name
@@ -27,6 +43,7 @@ const ContactForm = ({ onAddContact }) => {
             name="name"
             className={styles.input}
           />
+          <ErrorMessage name="name" component="span" />
         </label>
         <label htmlFor={frmNumberId} className={styles.label}>
           Number
@@ -36,6 +53,7 @@ const ContactForm = ({ onAddContact }) => {
             name="number"
             className={styles.input}
           />
+          <ErrorMessage name="number" component="span" />
         </label>
         <button type="submit" className={styles.btn}>
           Add contact
